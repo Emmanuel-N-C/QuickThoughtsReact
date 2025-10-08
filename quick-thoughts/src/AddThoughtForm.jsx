052da954a9ba8a37abb619747a1d8da39ad8d761
Emmanuel-N-C <nwang@DESKTOP-KI5KVCE>
2025-10-08 15:34:26 +0200

    first commit

---
+++ quick-thoughts/src/AddThoughtForm.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { generateId, getNewExpirationTime } from './utilities';
+
+export function AddThoughtForm({ addThought }) {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Only add a thought if there's text
+    if (text.length > 0) {
+      const newThought = {
+        id: generateId(),
+        text: text,
+        expiresAt: getNewExpirationTime(),
+      };
+
+      addThought(newThought);
+      setText(''); // Clear input after submission
+    }
+  };
+
+  return (
+    <form className="AddThoughtForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+        value={text}
+        onChange={handleTextChange}
+      />
+      <input type="submit" value="Add" />
+    </form>
+  );
+}
